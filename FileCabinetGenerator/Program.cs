7f9b976e7c862f885e21de570c3755743f68ef6a
alexei.putik <alexeiputik@yandex.by>
2020-04-11 14:48:02 +0300

    Create FileCabinetGenerator.exe

---
+++ FileCabinetGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetGenerator
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
